1ae3ff63e08e1a36ea19cce5d05d968de769d0e8
StevePro7 <steveprozune@hotmail.com>
2021-10-02 18:25:57 +0100

    new tests

--- GingkoUnitTesting/UnitTestingGinkgoSeries/01/main.go
+++ GingkoUnitTesting/UnitTestingGinkgoSeries/01/main.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+func Sum(x, y int) int {
+	return x + y
+}
+
 func main() {
+	sum := Sum(1, 2)
 	fmt.Println("ut")
+	fmt.Println(sum)
 }
--- GingkoUnitTesting/UnitTestingGinkgoSeries/01/main_test.go
+++ GingkoUnitTesting/UnitTestingGinkgoSeries/01/main_test.go
@@ -5,6 +5,8 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+
+	. "unittesting"
 )
 
 func TestUnitTestingGinkgoSeries(t *testing.T) {
@@ -12,6 +14,27 @@ func TestUnitTestingGinkgoSeries(t *testing.T) {
 	RunSpecs(t, "TestingDemo Suite")
 }
 
-var _ = Describe("01", func() {
+var _ = Describe("Sum", func() {
+
+	var (
+		p, q, m, n, sum1, sum2 int
+	)
+
+	BeforeEach(func() {
+		p, q, sum1 = 5, 6, 11
+		// putting wrong value of sum2 intentionally
+		m, n, sum2 = 8, 7, 16
+	})
+
+	Context("Addition of two digits", func() {
+		It("should return sum of the two digits", func() {
+			additionOfTwoDigits := Sum(p, q)
+			Expect(additionOfTwoDigits).Should(Equal(sum1))
+		})
+		It("should not return the sum provided", func() {
+			additionOfTwoDigits := Sum(m, n)
+			Expect(additionOfTwoDigits).ShouldNot(Equal(sum2))
+		})
+	})
 
 })

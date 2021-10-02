8539b9b5a097298a3225bce94e01204d27cb6eab
StevePro7 <steveprozune@hotmail.com>
2021-10-02 18:42:13 +0100

    New code

--- GingkoUnitTesting/UnitTestingGinkgoSeries/02/main.go
+++ GingkoUnitTesting/UnitTestingGinkgoSeries/02/main.go
@@ -1,13 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func Sum(x, y int) int {
 	return x + y
 }
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	_, err := fmt.Fprintf(w, "Hi there endpoint %s", r.URL.Path[1:])
+	if err != nil {
+		return
+	}
+}
+
+func ReadHandler(w http.ResponseWriter, _ *http.Request) {
+	dat, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	_, err = fmt.Fprintf(w, "Content in file is \r\n %s", string(dat))
+	if err != nil {
+		return
+	}
+}
+
 func main() {
-	sum := Sum(1, 2)
-	fmt.Println("ut")
-	fmt.Println(sum)
+	http.HandleFunc("/", Handler)
+	http.HandleFunc("/read", ReadHandler)
+	err := http.ListenAndServe(":8002", nil)
+	if err != nil {
+		return
+	}
 }

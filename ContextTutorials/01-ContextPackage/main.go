a922f677e211723611fb339d8b96b38e9e2dbee1
StevePro7 <steveprozune@hotmail.com>
2022-01-11 21:04:04 +0000

    context example

--- ContextTutorials/01-ContextPackage/main.go
+++ ContextTutorials/01-ContextPackage/main.go
@@ -1,29 +1,87 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-func printHello(ch chan int) {
-	fmt.Println("Hello from printHello")
-	ch <- 2
-	ch <- 3
+func sleepRandom(fromFunction string, ch chan int) {
+
+	defer func() {
+		fmt.Println(fromFunction, "sleepRandom complete")
+	}()
+
+	seed := time.Now().UnixNano()
+	r := rand.New(rand.NewSource(seed))
+	randomNumber := r.Intn(100)
+	sleeptime := randomNumber + 100
+	fmt.Println(fromFunction, "Starting sleep for ", sleeptime, "ms")
+	time.Sleep(time.Duration(sleeptime) * time.Millisecond)
+	fmt.Println(fromFunction, "Waking up, slept for ", sleeptime, "ms")
+
+	if ch != nil {
+		ch <- sleeptime
+	}
+}
+
+func sleepRandomContext(ctx context.Context, ch chan bool) {
+
+	defer func() {
+		fmt.Println("sleepRandomContext complete")
+		ch <- true
+	}()
+
+	sleeptimeChan := make(chan int)
+	go sleepRandom("sleepRandomContext", sleeptimeChan)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("sleepRandomContext: Time to return")
+
+	case sleeptime := <-sleeptimeChan:
+		fmt.Println("Slept for ", sleeptime, "ms")
+	}
+}
+
+func doWorkContext(ctx context.Context) {
+
+	ctxWithTimeout, cancelFunction := context.WithTimeout(ctx, time.Duration(150)*time.Millisecond)
+
+	defer func() {
+		fmt.Println("doWorkContext complete")
+		cancelFunction()
+	}()
+
+	ch := make(chan bool)
+	go sleepRandomContext(ctxWithTimeout, ch)
+
+	// Use a select statement to exit out if context expires
+	select {
+	case <-ctx.Done():
+		fmt.Println("doWorkContext: Time to return")
+	case <-ch:
+		fmt.Println("sleepRandomContext returned")
+	}
 }
 
 func main() {
 
-	ch := make(chan int)
+	ctx := context.Background()
+	ctxWithCancel, cancelFunction := context.WithCancel(ctx)
 
-	go func() {
-		fmt.Println("Hello inline")
-		ch <- 1
+	defer func() {
+		fmt.Println("Main defer : cancelling context")
+		cancelFunction()
 	}()
 
-	go printHello(ch)
-	fmt.Println("Hello from main")
+	go func() {
+		sleepRandom("Main", nil)
+		cancelFunction()
+		fmt.Println("Main sleep complete, cancelling context")
+	}()
 
-	i := <-ch
-	fmt.Println("Reeived ", i)
-
-	j := <-ch
-	fmt.Println("Reeived ", j)
-	<-ch
+	// Do work
+	doWorkContext(ctxWithCancel)
 }

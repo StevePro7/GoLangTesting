f92464f6727145a07ec8896ad77b1ecdac8abd29
StevePro7 <steveprozune@hotmail.com>
2022-01-11 20:46:52 +0000

    channels

--- ContextTutorials/01-ContextPackage/main.go
+++ ContextTutorials/01-ContextPackage/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
-func printHello() {
+func printHello(ch chan int) {
 	fmt.Println("Hello from printHello")
+	ch <- 2
 }
 
 func main() {
 
+	ch := make(chan int)
+
 	go func() {
 		fmt.Println("Hello inline")
+		ch <- 1
 	}()
 
-	go printHello()
-
+	go printHello(ch)
 	fmt.Println("Hello from main")
+
+	i := <-ch
+	fmt.Println("Reeived ", i)
+
+	<-ch
 }

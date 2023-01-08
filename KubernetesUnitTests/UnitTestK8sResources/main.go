d6221b7592b86350e2cfb908c501cabd60ef79c4
StevePro7 <steveprozune@hotmail.com>
2023-01-08 18:05:27 +0000

    all tests

--- KubernetesUnitTests/UnitTestK8sResources/main.go
+++ KubernetesUnitTests/UnitTestK8sResources/main.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"strings"
+)
 
-func uppercasePodLabel() (string, error) {
-	return "", nil
+func uppercasePodLabel(clientset kubernetes.Interface, namespace, podName, labelKey string) (string, error) {
+
+	pod, err := clientset.CoreV1().Pods(namespace).Get(
+		context.Background(),
+		podName,
+		v1.GetOptions{},
+	)
+	if err != nil {
+		return "", err
+	}
+
+	labelValue, ok := pod.ObjectMeta.Labels[labelKey]
+	if !ok {
+		return "", fmt.Errorf("no label with key %s for pod %s/%s", labelKey, namespace, podName)
+	}
+
+	return strings.ToUpper(labelValue), nil
 }
 
 func main() {
--- KubernetesUnitTests/UnitTestK8sResources/main_test.go
+++ KubernetesUnitTests/UnitTestK8sResources/main_test.go
@@ -1,29 +1,87 @@
 package main
 
 import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/client-go/kubernetes/fake"
 	"testing"
 )
 
 func TestLabelUpperCase(t *testing.T) {
 	testCases := []struct {
-		name          string
-		expectSuccess bool
+		name               string
+		pods               []runtime.Object
+		targetNamespace    string
+		targetPod          string
+		targetLabelKey     string
+		expectedLabelValue string
+		expectSuccess      bool
 	}{
 		{
-			name:          "existing pod",
-			expectSuccess: true,
+			name: "existing_pod_found",
+			pods: []runtime.Object{
+				&corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pod1",
+						Namespace: "namespace1",
+						Labels: map[string]string{
+							"label1": "value1",
+						},
+					},
+				},
+			},
+			targetNamespace:    "namespace1",
+			targetPod:          "pod1",
+			targetLabelKey:     "label1",
+			expectedLabelValue: "VALUE1",
+			expectSuccess:      true,
+		},
+		{
+			name:               "no_pods_existing",
+			pods:               []runtime.Object{},
+			targetNamespace:    "namespace1",
+			targetPod:          "pod1",
+			targetLabelKey:     "label1",
+			expectedLabelValue: "VALUE1",
+			expectSuccess:      false,
+		},
+		{
+			name: "existing_pod_missing_label",
+			pods: []runtime.Object{
+				&corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "pod1",
+						Namespace: "namespace1",
+					},
+				},
+			},
+			targetNamespace:    "namespace1",
+			targetPod:          "pod1",
+			targetLabelKey:     "label1",
+			expectedLabelValue: "VALUE1",
+			expectSuccess:      false,
 		},
 	}
 
 	for _, test := range testCases {
 		t.Run(test.name, func(t *testing.T) {
-			labelValue, err := uppercasePodLabel()
+			fakeClientset := fake.NewSimpleClientset(test.pods...)
+			labelValue, err := uppercasePodLabel(
+				fakeClientset,
+				test.targetNamespace,
+				test.targetPod,
+				test.targetLabelKey,
+			)
+
 			if err != nil && test.expectSuccess {
-				t.Fatalf("uxpected %v", err)
+				t.Fatalf("unexpected error getting label: %v", err)
 			} else if err == nil && !test.expectSuccess {
-				t.Fatalf("expect")
-			} else if labelValue != "bob" && !test.expectSuccess {
-				t.Fatalf("label %v", labelValue)
+				t.Fatalf("expected error but received none getting label")
+			} else if labelValue != test.expectedLabelValue && test.expectSuccess {
+				t.Fatalf("label value %s unexpectedly not equal to %s", labelValue, test.expectedLabelValue)
+			} else if labelValue == test.expectedLabelValue && !test.expectSuccess {
+				t.Fatalf("label values are unexpectedly equal: %s", labelValue)
 			}
 		})
 	}

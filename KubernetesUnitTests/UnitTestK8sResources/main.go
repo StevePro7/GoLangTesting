b97248efee42ed2e0ed666a3b7f59016b0067443
StevePro7 <steveprozune@hotmail.com>
2023-01-08 18:48:36 +0000

    update code

--- KubernetesUnitTests/UnitTestK8sResources/main.go
+++ KubernetesUnitTests/UnitTestK8sResources/main.go
@@ -5,6 +5,9 @@ import (
 	"fmt"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"os"
+	"path/filepath"
 	"strings"
 )
 
@@ -28,5 +31,33 @@ func uppercasePodLabel(clientset kubernetes.Interface, namespace, podName, label
 }
 
 func main() {
-	fmt.Println("unit testing resources")
+	fmt.Println("Get Kubernetes pod label to uppercase")
+
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Printf("error getting user home dir: %v\n", err)
+		os.Exit(1)
+	}
+
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	fmt.Printf("Using kubeconfig: %s\n", kubeConfigPath)
+
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		fmt.Printf("error getting Kubernetes config: %v\n", err)
+		os.Exit(1)
+	}
+
+	clientset, err := kubernetes.NewForConfig(kubeConfig)
+	if err != nil {
+		fmt.Printf("error getting Kubernetes clientset: %v\n", err)
+		os.Exit(1)
+	}
+
+	labelValue, err := uppercasePodLabel(clientset, "default", "testpod1", "hello")
+	if err != nil {
+		fmt.Printf("error getting pod label: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("Pod label value: %s\n", labelValue)
 }

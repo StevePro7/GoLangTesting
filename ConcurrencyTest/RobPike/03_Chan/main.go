b5ad805cb437ec87241c311d76aa670fbd6429ac
StevePro7 <steveprozune@hotmail.com>
2023-10-05 21:17:38 +0100

    goid

--- ConcurrencyTest/RobPike/03_Chan/main.go
+++ ConcurrencyTest/RobPike/03_Chan/main.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 	"math/rand"
 	"time"
+
+	"github.com/nikandfor/goid"
 )
 
 func boring(msg string, c chan string) {
 	for i := 0; ; i++ {
-		c <- fmt.Sprintf("%s %d", msg, i)
+		c <- fmt.Sprintf("[%d] %s %d", goid.ID(), msg, i)
 		time.Sleep(time.Duration(rand.Intn(1e3)) * time.Millisecond)
 	}
 }
 
 func main() {
 	c := make(chan string)
+	fmt.Println(goid.ID(), "beg")
 	go boring("boring", c)
 	for i := 0; i < 5; i++ {
 		fmt.Printf("say: %q\n", <-c)
 	}
-
-	fmt.Println("end")
+	fmt.Println(goid.ID(), "end")
 }

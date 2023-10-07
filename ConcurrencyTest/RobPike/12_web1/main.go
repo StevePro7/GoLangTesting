b357bb30c47136f69ea28026e7e970cf39fd0f24
StevePro7 <steveprozune@hotmail.com>
2023-10-07 15:33:22 +0100

    no message

--- ConcurrencyTest/RobPike/12_web1/main.go
+++ ConcurrencyTest/RobPike/12_web1/main.go
@@ -1,20 +1,34 @@
 package main
 
 import (
-	"crypto/rand"
 	"fmt"
+	"math/rand"
 	"time"
+
+	"github.com/nikandfor/goid"
 )
 
-type Result struct{}
+type Result struct {
+	text string
+}
 type Search func(query string) Result
+
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
 
 func fakeSearch(kind string) Search {
 	return func(query string) Result {
-		time.Sleep(time.Duration(rand.Intn(100) * time.Millisecond)
-		return Result
+		duration := time.Duration(rand.Intn(100)) * time.Millisecond
+		time.Sleep(duration)
+		msg := fmt.Sprintf("[%d] %s result for %q\n", goid.ID(), kind, query)
+		return Result{msg}
 	}
 }
 func main() {
-	fmt.Println("bob")
+	fmt.Printf("[%d] beg\n", goid.ID())
+
+	fmt.Printf("[%d] end\n", goid.ID())
 }

822e6d2d9ae3cb556a815e547bdd3ee5807bda39
StevePro7 <steveprozune@hotmail.com>
2023-10-07 15:50:28 +0100

    no message

--- ConcurrencyTest/RobPike/13_web2/main.go
+++ ConcurrencyTest/RobPike/13_web2/main.go
@@ -20,9 +20,15 @@ var (
 )
 
 func Google(query string) (results []Result) {
-	results = append(results, Web(query))
-	results = append(results, Image(query))
-	results = append(results, Video(query))
+	c := make(chan Result)
+	go func() { c <- Web(query) }()
+	go func() { c <- Image(query) }()
+	go func() { c <- Video(query) }()
+
+	for i := 0; i < 3; i++ {
+		result := <-c
+		results = append(results, result)
+	}
 	return
 }
 

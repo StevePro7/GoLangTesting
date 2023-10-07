32d7df01f7ea5adfd298b5d2334b4e5321dfd316
StevePro7 <steveprozune@hotmail.com>
2023-10-07 20:47:00 +0100

    no message

--- ConcurrencyTest/SameerAdv/04_buf/main.go
+++ ConcurrencyTest/SameerAdv/04_buf/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/nikandfor/goid"
 )
@@ -13,25 +12,40 @@ type Ball struct {
 
 func main() {
 	fmt.Printf("[%d] beg\n", goid.ID())
-	table := make(chan *Ball)
-	go player("ping", table)
-	go player("pong", table)
-
-	table <- new(Ball)
-	time.Sleep(1 * time.Second)
-	<-table
-
+	in, out := make(chan int), make(chan int)
+	go buffer(in, out)
+	for i := 0; i < 10; i++ {
+		in <- i
+	}
+	close(in)
+	for i := range out {
+		fmt.Printf("[%d] %d\n", goid.ID(), i)
+	}
 	fmt.Printf("[%d] end\n", goid.ID())
 }
 
-func player(name string, table chan *Ball) {
-	for {
-		recd := <-table
-		ball := recd
-		ball.hits++
-		fmt.Printf("[%d] %s %d\n", goid.ID(), name, ball.hits)
-		time.Sleep(100 * time.Millisecond)
-		send := ball
-		table <- send
+// buffer provides an unbounded buffer between in and out.
+// buffer exits when it is closed and all items in the
+// buffer have been sent to out at which point closes
+func buffer(in <-chan int, out chan<- int) {
+	var buf []int
+	for in != nil || len(buf) > 0 {
+		var i int
+		var c chan<- int
+		if len(buf) > 0 {
+			i = buf[0]
+			c = out // enable send case
+		}
+		select {
+		case n, ok := <-in:
+			if ok {
+				buf = append(buf, n)
+			} else {
+				in = nil // disable reseive case
+			}
+		case c <- i:
+			buf = buf[1:]
+		}
 	}
+	close(out)
 }

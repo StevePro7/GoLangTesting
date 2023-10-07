7f6d3f3ca954e3c29e4d9adc9c88b55d5f0e5189
StevePro7 <steveprozune@hotmail.com>
2023-10-07 16:12:00 +0100

    no message

---
+++ ConcurrencyTest/RobPike/14_web3/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/nikandfor/goid"
+)
+
+type Result struct {
+	text string
+}
+type Search func(query string) Result
+
+var (
+	Web   = fakeSearch("web")
+	Image = fakeSearch("image")
+	Video = fakeSearch("video")
+)
+
+func Google(query string) (results []Result) {
+	c := make(chan Result)
+	go func() { c <- Web(query) }()
+	go func() { c <- Image(query) }()
+	go func() { c <- Video(query) }()
+
+	for i := 0; i < 3; i++ {
+		result := <-c
+		results = append(results, result)
+	}
+	return
+}
+
+func fakeSearch(kind string) Search {
+	return func(query string) Result {
+		duration := time.Duration(rand.Intn(100)) * time.Millisecond
+		time.Sleep(duration)
+		msg := fmt.Sprintf("[%d] %s result for %q\n", goid.ID(), kind, query)
+		return Result{msg}
+	}
+}
+func main() {
+	fmt.Printf("[%d] beg\n", goid.ID())
+	rand.Seed(time.Now().UnixNano())
+	start := time.Now()
+	results := Google("golang")
+	elapsed := time.Since(start)
+	fmt.Printf("[%d] %q\n", goid.ID(), results)
+	fmt.Printf("[%d] %q\n", goid.ID(), elapsed)
+	fmt.Printf("[%d] end\n", goid.ID())
+}

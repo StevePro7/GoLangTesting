f2264a929505d24e715705faa4f9b2e069099c8a
StevePro7 <steveprozune@hotmail.com>
2023-11-05 21:30:37 +0000

    no message

---
+++ EventTesting/EventStreaming/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/segmentio/kafka-go"
+)
+
+func main() {
+	// Set up a Kafka producer to send events to a topic
+	writer := kafka.NewWriter(kafka.WriterConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "my-topic",
+	})
+
+	// Send some events to the topic
+	writer.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   []byte("key1"),
+			Value: []byte("value1"),
+		},
+		kafka.Message{
+			Key:   []byte("key2"),
+			Value: []byte("value2"),
+		},
+	)
+
+	// Set up a Kafka consumer to read events from the topic
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: []string{"localhost:9092"},
+		Topic:   "my-topic",
+	})
+
+	// Read events from the topic
+	for {
+		msg, err := reader.ReadMessage(context.Background())
+		if err != nil {
+			break
+		}
+
+		fmt.Printf("Rec'd msg: key=%s, val=%s\n", string(msg.Key), string(msg.Value))
+	}
+}

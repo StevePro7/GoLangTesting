3779653ac9a37c4162e8b7ee3441debcbb241f02
StevePro7 <steveprozune@hotmail.com>
2023-11-17 20:34:31 +0000

    no message

---
+++ InterviewQs/Crack50InterviewQs/05/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func Merge(left, rght []int) []int {
+	merged := make([]int, 0, len(left)+len(rght))
+	for len(left) > 0 || len(rght) > 0 {
+		if len(left) == 0 {
+			return append(merged, rght...)
+		} else if len(rght) == 0 {
+			return append(merged, left...)
+		} else if left[0] < rght[0] {
+			merged = append(merged, left[0])
+			left = left[1:]
+		} else {
+			merged = append(merged, rght[0])
+			rght = rght[1:]
+		}
+	}
+
+	return merged
+}
+
+func MergeSort(data []int) []int {
+	if len(data) <= 1 {
+		return data
+	}
+	done := make(chan bool)
+	midd := len(data) / 2
+	var left []int
+	go func() {
+		left = MergeSort(data[:midd])
+		done <- true
+	}()
+	rght := MergeSort(data[midd:])
+	<-done
+	return Merge(left, rght)
+}
+
+func main() {
+	println("beg")
+	data := []int{9, 4, 3, 6, 1, 2, 10, 5, 7, 8}
+	fmt.Printf("%v\n%v\n", data, MergeSort(data))
+	//fmt.Printf("%v\n%v\n", data, data)
+	println("end")
+}

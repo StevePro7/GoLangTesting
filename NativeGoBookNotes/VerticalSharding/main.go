dfae3c3e9d9e9850dcf70f1e63a128b805254fc3
StevePro7 <steveprozune@hotmail.com>
2023-11-26 20:36:08 +0000

    coding

--- NativeGoBookNotes/VerticalSharding/main.go
+++ NativeGoBookNotes/VerticalSharding/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"golangtesting/sharded"
 	"sync"
+	"time"
 )
 
 type unshardedMap struct {
@@ -17,5 +19,52 @@ func newUnshardedMap() *unshardedMap {
 }
 
 func main() {
-	fmt.Println("hello??")
+	unshardedMap := newUnshardedMap()
+	t := time.Now()
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < 1000000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			unshardedMap.Lock()
+			unshardedMap.m[fmt.Sprintf("key%d", i)] = i
+			defer unshardedMap.Unlock()
+		}(i)
+	}
+
+	for i := 0; i < 1000000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			unshardedMap.RLock()
+			_ = unshardedMap.m[fmt.Sprintf("key%d", i)]
+			defer newUnshardedMap().RUnlock()
+		}(i)
+	}
+
+	wg.Wait()
+
+	fmt.Println(time.Since(t))
+	t = time.Now()
+
+	shardedMap := sharded.NewShardedMap(10)
+	for i := 0; i < 1000000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			shardedMap.Set(fmt.Sprintf("key%d", i), i)
+		}(i)
+	}
+
+	for i := 0; i < 1000000; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			_ = shardedMap.Get(fmt.Sprintf("key%d", i))
+		}(i)
+	}
+
+	wg.Wait()
+	fmt.Println(time.Since(t))
 }
